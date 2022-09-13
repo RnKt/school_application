1f5514a541518a31eaa42d00a40878e59ce8ecbf
RnKt <79905111+RnKt@users.noreply.github.com>
2022-09-13 22:56:42 +0200

    first setup

---
+++ resources/views/admin/parts/sidemenu.blade.php
@@ -0,0 +1,10 @@
+<div>
+  <a href="">
+    <img src="" alt="logo">
+  </a>
+  <ul>
+    <li>
+      <a href="">text</a>
+    </li>
+  </ul>
+</div>
--- routes/admin.php
+++ routes/admin.php
@@ -1,12 +1,13 @@
 <?php
 
+use App\Http\Controllers\Admin\AuthController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Admin\OverviewController;
 
 Route::group(['as' => 'admin.'], function () {
-    
-    Route::get('/', [OverviewController::class, 'index'])->name('home');
-   
+    Route::group(['middleware' => ['auth.admin:admin']], function () {
+      
+        Route::get('/', [OverviewController::class, 'index'])->name('home');
+    });
 
     Route::get('/login', [AuthController::class, 'login'])->name('login');
     Route::post('/login', [AuthController::class, 'loginPost']);
