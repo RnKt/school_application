5275f8f664369e574fabb5859ecd8dee2a0e0232
SamuelTomic <samuel.tomic03@gmail.com>
2022-11-13 22:38:40 +0100

    code

--- resources/views/client/login/code.blade.php
+++ resources/views/client/login/code.blade.php
@@ -13,7 +13,13 @@
         <img src="" alt="">
         <h3>Stredna Priemyselna skola Elektrotechnicka</h3>
       </div>
-
-
-    </form>
+      <div class="form_applicant-container">
+      <h3>Zadaj svoj kod</h3>
+      <div class="form_applicant-input_box">
+          <input type="Text" name="zadaj_kod">
+          <img class="form_applicant-submit_img" type ="image" src="" alt="Submit">
+        </div>
+     </div>
+    </div>
+  </form>
 @endsection
